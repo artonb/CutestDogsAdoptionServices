a0506108a6db5ba7fdde3f6c5f02756738b23eb2
artonb <146065746+artonb@users.noreply.github.com>
2024-01-31 11:40:51 +0100

    add views

--- CutestDogsAdoptionServices/Models/DogModel.cs
+++ CutestDogsAdoptionServices/Models/DogModel.cs
@@ -1,6 +1,17 @@
-﻿namespace CutestDogsAdoptionServices.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CutestDogsAdoptionServices.Models
 {
-    public class DogModel
-    {
-    }
+	public class DogModel
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Cuteness { get; set; }
+		public string Image { get; set; }
+		public string FavFood { get; set; }
+		public string FavToy { get; set; }
+		public int Temperament { get; set; }
+		public bool IsAdopted { get; set; }
+	}
 }
